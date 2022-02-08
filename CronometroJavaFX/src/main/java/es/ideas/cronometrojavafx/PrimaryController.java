c3d6de4541d230e037f0ac32c8260e0841156997
iAleZz <5604501@alu.murciaeduca.es>
2022-02-08 14:08:55 +0100

    Se han añadido bastantes mejoras al programa, tanto en la distribución de los componentes, como en el funcionamiento de los mismos, añadiendo binds para y otros

--- CronometroJavaFX/src/main/java/es/ideas/cronometrojavafx/PrimaryController.java
+++ CronometroJavaFX/src/main/java/es/ideas/cronometrojavafx/PrimaryController.java
@@ -1,18 +1,20 @@
 package es.ideas.cronometrojavafx;
 
 import java.net.URL;
-import java.security.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.util.Duration;
 
 public class PrimaryController implements Initializable{
@@ -24,47 +26,80 @@ public class PrimaryController implements Initializable{
     @FXML
     private Button btnReiniciar;
     @FXML
+    private Button btnReanudar;
+    @FXML
     private Label lbCronometro;
     @FXML
     private Label lbReloj;
+    @FXML
+    private TextField tfHoras;
+    @FXML
+    private TextField tfMinutos;
+    @FXML
+    private TextField tfSegundos;
     
     private Timeline cronometro;
-    private int segundos = 0;
-    private int minutos = 0;
-    private int horas = 0;
-    
     private Timeline reloj;
+    
+    private int segundos;
+    private int minutos;
+    private int horas;
+    
     private final SimpleDateFormat formato = new SimpleDateFormat("HH:mm:ss");
+  /* Utilizamos una SimpleBooleanProperty para controlar el botonIniciar de una
+    forma más comoda */
+    private SimpleBooleanProperty booleanIniciar = new SimpleBooleanProperty(true);
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //Necesario para que el cronometro empiece en 0:0:0
-        actualizarCronometro();
-        
-        //Timeline del reloj (hora actual)
-        reloj = new Timeline();
-        reloj.setCycleCount(Animation.INDEFINITE);
-        reloj.getKeyFrames().add(new KeyFrame(Duration.millis(1000), (ActionEvent evento) -> {
-            lbReloj.setText(formato.format(new Date()));
-        }));
-        reloj.play();
-        
-        //Timeline del cronometro
+        //Timeline del cronómetro
         cronometro = new Timeline();
         cronometro.setCycleCount(Animation.INDEFINITE);
-        cronometro.getKeyFrames().add(new KeyFrame(Duration.millis(1000), (ActionEvent evento) -> {
+        cronometro.getKeyFrames().add(new KeyFrame(Duration.millis(1000), 
+                (ActionEvent evento) -> {
             segundos--;
             comprobarFormato();
             actualizarCronometro();
         }));
         
+        //Timeline del reloj (hora actual)
+        reloj = new Timeline();
+        reloj.setCycleCount(Animation.INDEFINITE);
+        reloj.getKeyFrames().add(new KeyFrame(Duration.millis(1000), 
+                (ActionEvent evento) -> {
+            lbReloj.setText(formato.format(new Date()));
+        }));
+        reloj.play();
+        
+        //Controlamos que los TextField admitan solo números
+        tfSegundos.textProperty().addListener((ObservableValue<? extends 
+                String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfHoras.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        tfMinutos.textProperty().addListener((ObservableValue<? extends 
+                String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfHoras.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        }); 
+        tfHoras.textProperty().addListener((ObservableValue<? extends 
+                String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfHoras.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });        
+        
+        /* Deshabilita el botón de Añadir cuando los parámetros
+           horas, minutos y segundos no se han introducido aún*/
+        btnIniciar.disableProperty().
+                bind(tfSegundos.textProperty().isEmpty().
+                or(tfMinutos.textProperty().isEmpty().
+                or(tfHoras.textProperty().isEmpty()    
+                )));
     }
-    
-    //Método para actualizar el label del crónometro.
-    public void actualizarCronometro(){
-        lbCronometro.setText(horas + ":" + minutos + ":" + segundos);
-    }
-    
+      
     /* Método para comprobar el formato del cronómetro.
        Tiene en cuenta que si el siguiente segundo es el -1, los segundos pasan
        a ser de nuevo 59 y actualiza los minutos restándole 1.
@@ -76,25 +111,71 @@ public class PrimaryController implements Initializable{
             segundos=59;
             minutos--;
         }
+        
         if (minutos==-1){
             minutos=59;
             horas--;
         }
+        
+        if(horas == 0 && minutos == 0 && segundos == 0){
+            cronometro.stop();
+            //Añadir reproducción de sonido o alerta
+        }
     }
-
+   
+  /*Método para actualizar la label del crónometro.
+    Tiene en cuenta que si las horas, minutos o segundos son inferiores a 10,
+    nos añade un 0 delante del número para que de un aspecto más real */
+    public void actualizarCronometro(){
+        String texto = (horas<=9?"0":"")+horas+":"+(minutos<=9?"0":"")+minutos+":"+(segundos <= 9?"0":"")+segundos;
+        lbCronometro.setText(texto);
+    }
+       
     //OnAction para los botones de iniciar, parar y reiniciar cronómetro.
     @FXML
     private void iniciarCrono(ActionEvent event) {
+        //Convierte los números introducidos en los textfield a "int"
+        segundos = Integer.parseInt(tfSegundos.getText());
+        minutos = Integer.parseInt(tfMinutos.getText());
+        horas = Integer.parseInt(tfHoras.getText());
+        
+        /*Bind para habilitar/deshabilitar el botón Iniciar teniendo en cuenta
+          si los textfield están rellenados o no */
+        booleanIniciar.set(false);
+        btnIniciar.disableProperty().bind((
+                tfSegundos.textProperty().isEmpty()).
+                or(tfMinutos.textProperty().isEmpty()).
+                or(tfHoras.textProperty().isEmpty()).
+                or(booleanIniciar.not()));
+        
         cronometro.play();
+        tfSegundos.setDisable(true);
+        tfMinutos.setDisable(true);
+        tfHoras.setDisable(true);
     }
 
+    //Método para pausar el cronómetro
     @FXML
     private void pararCrono(ActionEvent event) {
         cronometro.pause();
     }
+    
+    //Método para reanudar el cronómetro
+    @FXML
+    private void reanudarCrono(ActionEvent event) {
+        cronometro.play();
+    }
 
+    //Método para resetear el cronómetro y poner todo a 0 como en un inicio.
     @FXML
     private void reiniciarCrono(ActionEvent event) {
+        segundos = 0; tfSegundos.setText(""); tfSegundos.setDisable(false);
+        minutos = 0; tfMinutos.setText(""); tfMinutos.setDisable(false);
+        horas = 0; tfHoras.setText(""); tfHoras.setDisable(false);
+                
+        booleanIniciar.set(true);
+        cronometro.stop();
+        comprobarFormato();
         actualizarCronometro();
     }
 }
