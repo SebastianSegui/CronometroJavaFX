8123d0c21342f748696fe2d1d0bf0ddb67b91c13
Sebastian Segui Sanchez <sebas@correo.com>
2022-02-15 17:43:52 +0100

    Sonido e imagenes implementadas

--- CronometroJavaFX/src/main/java/module-info.java
+++ CronometroJavaFX/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module es.ideas.cronometrojavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    requires java.desktop;
+    
     opens es.ideas.cronometrojavafx to javafx.fxml;
     exports es.ideas.cronometrojavafx;
 }
